84d0ceceb87f56aedc4e25c80f17f30414778c90
taichi221228 <taichi221228@icloud.com>
2022-06-22 14:31:58 +0900

    Add a server script

--- app/server.js
+++ app/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-console.log(process.env.PORT)
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Hello world listening on port', port);
