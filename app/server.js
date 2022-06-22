60935e51b8314453009b8a1635b63e7e14b7b95c
taichi221228 <taichi221228@icloud.com>
2022-06-22 17:09:55 +0900

    Change the directory structure

--- app/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Hello world listening on port', port);
-});
