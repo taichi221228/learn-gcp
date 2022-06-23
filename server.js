62e8b3b8de70b7731e2faf0b4da6a634ea28e1b4
taichi221228 <taichi221228@icloud.com>
2022-06-23 14:41:57 +0900

    Add a IP validation method

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const port = parseInt(process.env.PORT) || 8080;
+const permittedIpList = ['127.0.0.1'];
+
+// app.use((req, res, next) => {
+//   permittedIpList.map((permittedIp) => {
+//     if (req.ip === permittedIp) {
+//       next();
+//     }
+//   });
+// });
 
 app.use(express.static('build'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('build'));
-});
+app.get('/', (req, res) => res.sendFile(path.resolve('build')));
 
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-});
+app.listen(port);
