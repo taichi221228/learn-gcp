4ceeed5abe6b47a90adfb0d11a28852ecf4aed5c
taichi221228 <taichi221228@icloud.com>
2022-06-23 19:02:10 +0900

    Test for Cloud Run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This app is hosted by GCP.
+          This app is bootstrapped by CRA and hosted by GCP.
         </p>
         <a
           className="App-link"
