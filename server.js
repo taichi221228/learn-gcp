58e2d4d3dd74cedf26edc775f5b14bf76c01aa5a
taichi221228 <taichi221228@icloud.com>
2022-06-23 17:19:42 +0900

    Refactor code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = parseInt(process.env.PORT) || 8080;
+const buildDir = 'build';
 // const permittedIpList = ['127.0.0.1'];
 
 // app.use((req, res, next) => {
@@ -12,8 +13,8 @@ const port = parseInt(process.env.PORT) || 8080;
 //   });
 // });
 
-app.use(express.static('build'));
+app.use(express.static(buildDir));
 
-app.get('/', (req, res) => res.sendFile(path.resolve('build')));
+app.get('/', (req, res) => res.sendFile(path.resolve(buildDir)));
 
 app.listen(port);
