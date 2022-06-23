b7286971742b5db34abc19e9f4262a2571f1024c
taichi221228 <taichi221228@icloud.com>
2022-06-23 18:29:01 +0900

    Test for Cloud Run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This app is hosted by GCP.
+          This React app is hosted by GCP.
         </p>
         <a
           className="App-link"
