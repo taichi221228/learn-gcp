b55358174a6a7327c6892cbc0864d01d776b36cb
taichi221228 <taichi221228@icloud.com>
2022-06-23 15:25:11 +0900

    Hide a unnecessary code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = parseInt(process.env.PORT) || 8080;
-const permittedIpList = ['127.0.0.1'];
+// const permittedIpList = ['127.0.0.1'];
 
 // app.use((req, res, next) => {
 //   permittedIpList.map((permittedIp) => {
